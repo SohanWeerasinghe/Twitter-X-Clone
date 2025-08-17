c4ad690c13c3d995afe37fa344d30ebc78022b12
Sohan Weerasinghe <145052635+SohanWeerasinghe@users.noreply.github.com>
2025-08-17 15:24:30 +0530

    Project setup

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { login, register, logout } from "../controllers/auth.controller.js";
+import dotenv from "dotenv";  // Import dotenv to manage environment variables
+
+dotenv.config();  // Load environment variables from .env file
 
 const router = express.Router();
+console.log(process.env.MONGO_URI);     // Log the MONGO_URI to verify it's loaded correctly
 
 router.get("/login", login);
 
